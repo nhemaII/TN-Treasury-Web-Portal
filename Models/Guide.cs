d0b890f0d4067b10ec206cb9ad503615c3bf5bd5
nhemaII <nainoa.ik.hema@gmail.com>
2024-03-22 02:54:16 -0500

    Homepage styling and started guide feature

---
+++ Models/Guide.cs
@@ -0,0 +1,14 @@
+﻿namespace TN_Treasury_Web_Portal.Models
+{
+    public class Guide
+    {
+
+        public int Id { get; set; }
+        
+        public string Title { get; set; }
+
+        public string Description { get; set; }
+
+        public ICollection<Guide> Guides { get; set; } = new List<Guide>();
+    }
+}
---
+++ Models/GuideSection.cs
@@ -0,0 +1,17 @@
+﻿namespace TN_Treasury_Web_Portal.Models
+{
+    public class GuideSection
+    {
+
+        public int Id { get; set; }
+
+        public string Title { get; set; }
+
+        public string Content { get; set; }
+
+        public int GuideId { get; set; }
+
+        public Guide Guide { get; set; } = null!;
+
+    }
+}
