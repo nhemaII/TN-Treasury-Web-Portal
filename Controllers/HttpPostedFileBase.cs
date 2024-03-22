5cf944f4c75a987337eb3b68bcdaba14799b662d
theColetrain0117 <njcole93@gmail.com>
2024-03-22 08:10:04 -0500

    User Interaction
    Controller that allows users to interact with the "Document" section of the interface

---
+++ Controllers/HttpPostedFileBase.cs
@@ -0,0 +1,19 @@
+﻿
+namespace TN_Treasury_Web_Portal.Controllers
+{
+    public class HttpPostedFileBase
+    {
+        internal readonly int ContentLength;
+        internal string? FileName;
+
+        internal void SaveAs(string path)
+        {
+            throw new NotImplementedException();
+        }
+
+        internal void SaveAs(object path)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
